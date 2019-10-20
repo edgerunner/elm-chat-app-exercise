8e9bb4bf62f1ac79925c335465aa31aa6574381b
Mert Torun <mert@merttorun.com>
2019-10-20 18:12:15 +0300

    Extract convListing and unreadBadge from Chat to Conversation

--- src/Chat.elm
+++ src/Chat.elm
@@ -2,14 +2,14 @@ module Chat exposing (Model, Msg, init, subscriptions, update, view)
 
 import Browser.Dom
 import Browser.Events
-import Conversation exposing (Conversation)
+import Conversation exposing (Conversation, convListing)
 import Dict exposing (Dict)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Events as Events
 import Element.Font as Font
-import Styles exposing (em, eml, gray, red, white)
+import Styles exposing (em, eml, gray)
 import Task
 import User exposing (User, userLabel)
 
@@ -177,54 +177,19 @@ convList (Model model) =
             (\conv ->
                 let
                     user =
-                        userById model.users conv.with
+                        Dict.get conv.with model.users
+
+                    listing justUser =
+                        el
+                            [ Events.onClick (FocusConversation conv)
+                            , width fill
+                            ]
+                            (convListing conv justUser)
                 in
-                Maybe.map (convListing conv) user
+                Maybe.map listing user
             )
             model.conversations
         )
-
-
-convListing : Conversation -> User -> Element Msg
-convListing conv user =
-    row
-        [ width fill
-        , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
-        , spacing (em 1)
-        , paddingXY (em 0.25) 0
-        , Events.onClick <| FocusConversation conv
-        ]
-        [ userLabel user
-        , unreadBadge conv.unread
-        , text "❯"
-        ]
-
-
-unreadBadge : Int -> Element msg
-unreadBadge count =
-    case count of
-        0 ->
-            Element.none
-
-        _ ->
-            String.fromInt count
-                |> Element.text
-                |> el
-                    [ Font.bold
-                    , Font.size (em 1)
-                    , Font.color white
-                    , Border.rounded (em 1)
-                    , Background.color red
-                    , width (minimum (em 1.5) shrink)
-                    , height (eml 1.5)
-                    , padding (em 0.25)
-                    , Font.center
-                    ]
-
-
-userById : Dict String User -> String -> Maybe User
-userById users id =
-    Dict.get id users
 
 
 conversationView : Model -> Element Msg
--- src/Conversation.elm
+++ src/Conversation.elm
@@ -1,8 +1,15 @@
-module Conversation exposing (Conversation, get)
+module Conversation exposing (Conversation, convListing, get)
 
 import Api
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Events
+import Element.Font as Font
 import Json.Decode as D
 import RemoteData exposing (WebData)
+import Styles exposing (em, eml, red, white)
+import User exposing (User, userLabel)
 
 
 type alias Conversation =
@@ -24,3 +31,39 @@ decoder =
         (D.field "with_user_id" D.string)
         (D.field "unread_message_count" D.int)
         |> D.list
+
+
+convListing : Conversation -> User -> Element msg
+convListing conv user =
+    row
+        [ width fill
+        , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
+        , spacing (em 1)
+        , paddingXY (em 0.25) 0
+        ]
+        [ userLabel user
+        , unreadBadge conv.unread
+        , text "❯"
+        ]
+
+
+unreadBadge : Int -> Element msg
+unreadBadge count =
+    case count of
+        0 ->
+            Element.none
+
+        _ ->
+            String.fromInt count
+                |> Element.text
+                |> el
+                    [ Font.bold
+                    , Font.size (em 1)
+                    , Font.color white
+                    , Border.rounded (em 1)
+                    , Background.color red
+                    , width (minimum (em 1.5) shrink)
+                    , height (eml 1.5)
+                    , padding (em 0.25)
+                    , Font.center
+                    ]
