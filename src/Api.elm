1259d286c078f532f2e40b5449b0534cfc10bccd
Mert Torun <mert@merttorun.com>
2021-06-03 13:00:54 +0300

    Switch to the Mockaroo API

--- src/Api.elm
+++ src/Api.elm
@@ -2,15 +2,20 @@ module Api exposing (get)
 
 import Http
 import Json.Decode as D
-import RemoteData exposing (WebData, fromResult)
+import RemoteData exposing (WebData)
 
 
 get : String -> D.Decoder typ -> (WebData typ -> msg) -> Cmd msg
 get endpoint decoder msg =
-    Http.get
-        { url = "/api" ++ endpoint ++ ".json"
+    Http.request
+        { method = "GET"
+        , headers = [ Http.header "X-API-Key" "872a6cc0" ]
+        , url = "https://my.api.mockaroo.com" ++ endpoint ++ ".json"
+        , body = Http.emptyBody
         , expect =
             Http.expectJson
-                (fromResult >> msg)
+                (RemoteData.fromResult >> msg)
                 decoder
+        , timeout = Nothing
+        , tracker = Nothing
         }
